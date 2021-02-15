347fc29d53aafb4a34c84ad18f156f2f34f6dd0a
Cozarkian <77899393+Cozarkian@users.noreply.github.com>
2021-02-14 22:33:04 -0800

    Add files via upload

---
+++ Source/ModSettings_Entomophagy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Entomophagy
+{
+    public class ModSettings_Entomophagy : ModSettings
+    {
+        public static bool insectRecipes = false;
+        public static bool replaceMeals = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref insectRecipes, "insectRecipes", false);
+            Scribe_Values.Look(ref replaceMeals, "replaceMeals", true);
+            base.ExposeData();
+        }
+
+    }
+}
---
+++ Source/Thoughts_EatInsects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Entomophagy
+{
+    [HarmonyPatch]
+    public class Thoughts_EatInsects
+    {
+        [HarmonyPatch(typeof(FoodUtility), "ThoughtsFromIngesting")]
+        private static void Postfix(ref List<ThoughtDef> __result, Pawn ingester)
+        {
+            //Entomophagous
+            if (ingester.story.traits.HasTrait(TraitDef.Named("Entomophagous")))
+            {
+                if (__result.Contains(ThoughtDefOf.AteInsectMeatAsIngredient))
+                {
+                    __result.Remove(ThoughtDefOf.AteInsectMeatAsIngredient);
+                    __result.Add(ThoughtDef.Named("AteInsectMeatAsEntomophagous"));
+                }
+                if (__result.Contains(ThoughtDefOf.AteInsectMeatDirect))
+                {
+                    __result.Remove(ThoughtDefOf.AteInsectMeatDirect);
+                    __result.Add(ThoughtDef.Named("AteInsectMeatAsEntomophagous"));
+                }
+            }
+        }
+    }
+}
