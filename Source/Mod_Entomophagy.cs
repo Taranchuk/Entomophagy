ebe3dcd1434bb484fcfb446e54e9bf6202839925
Cozarkian <77899393+Cozarkian@users.noreply.github.com>
2021-10-21 16:11:41 -0700

    Add files via upload

--- Source/Mod_Entomophagy.cs
+++ Source/Mod_Entomophagy.cs
@@ -21,22 +21,22 @@ namespace Entomophagy
         {
             Rect rect = new Rect(50f, 50f, inRect.width * .8f, inRect.height);
             listingStandard.Begin(rect);
-            if (listingStandard.RadioButton_NewTemp("(Auto Meals):      Automatically create insect meals when using insect meat:", ModSettings_Entomophagy.replaceMeals && !ModSettings_Entomophagy.insectRecipes))
+            if (listingStandard.RadioButton("(Auto Meals):      Automatically create insect meals when using insect meat:", ModSettings_Entomophagy.replaceMeals && !ModSettings_Entomophagy.insectRecipes))
             {
                 ModSettings_Entomophagy.replaceMeals = true;
                 ModSettings_Entomophagy.insectRecipes = false;
             }
-            if (listingStandard.RadioButton_NewTemp("(Recipes):           Add insect meal recipes to cooking facilities:", !ModSettings_Entomophagy.replaceMeals && ModSettings_Entomophagy.insectRecipes))
+            if (listingStandard.RadioButton("(Recipes):           Add insect meal recipes to cooking facilities:", !ModSettings_Entomophagy.replaceMeals && ModSettings_Entomophagy.insectRecipes))
             {
                 ModSettings_Entomophagy.replaceMeals = false;
                 ModSettings_Entomophagy.insectRecipes = true;
             }
-            if (listingStandard.RadioButton_NewTemp("(Both):                Add recipes and auto replace meals:", ModSettings_Entomophagy.replaceMeals && ModSettings_Entomophagy.insectRecipes))
+            if (listingStandard.RadioButton("(Both):                Add recipes and auto replace meals:", ModSettings_Entomophagy.replaceMeals && ModSettings_Entomophagy.insectRecipes))
             {
                 ModSettings_Entomophagy.replaceMeals = true;
                 ModSettings_Entomophagy.insectRecipes = true;
             }
-            if (listingStandard.RadioButton_NewTemp("(Trait Only):        No recipes or meals:", !ModSettings_Entomophagy.replaceMeals && !ModSettings_Entomophagy.insectRecipes))
+            if (listingStandard.RadioButton("(Trait Only):        No recipes or meals:", !ModSettings_Entomophagy.replaceMeals && !ModSettings_Entomophagy.insectRecipes))
             {
                 ModSettings_Entomophagy.replaceMeals = false;
                 ModSettings_Entomophagy.insectRecipes = false;
